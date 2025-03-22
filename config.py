9eb381509aaebf1f2984e57b4a600dc5ff2a062f
m0shaban <20194312@csi.edu.eg>
2025-03-22 20:05:40 +0200

    Update configuration for Render deployment

--- config.py
+++ config.py
@@ -6,9 +6,9 @@ load_dotenv()
 class Config:
     SECRET_KEY = os.environ.get('SECRET_KEY') or 'sk-191a0bcb7a3d4a85b8ddd42373212426'
     
-    # Support for Render and other platforms
+    # Handle different database URLs
     database_url = (
-        os.environ.get('RENDER_DATABASE_URL') or
+        os.environ.get('RENDER_POSTGRES_URL') or
         os.environ.get('DATABASE_URL') or
         'sqlite:///' + os.path.join(os.path.abspath(os.path.dirname(__file__)), 'instance/db.sqlite')
     )
